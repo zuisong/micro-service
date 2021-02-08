4c91ace57d60f27f5f782ef71947836e33c075d8
chenjian <com.me@foxmail.com>
2021-02-08 13:51:25 +0800

    spock parcel runner config

--- micro-service-hello-client/src/test/groovy/cn/mmooo/HelloControllerSpec.groovy
+++ micro-service-hello-client/src/test/groovy/cn/mmooo/HelloControllerSpec.groovy
@@ -2,14 +2,22 @@ package cn.mmooo
 
 import spock.lang.Specification
 
+import java.time.LocalDateTime
+
 class HelloControllerSpec extends Specification {
 
-  def "test"() {
-
+  def "test1"() {
     expect:
-    println("=--= hello client ")
+    Thread.sleep(1000)
+    println("=--= hello test1 " + LocalDateTime.now())
     true
+  }
 
+  def "test2"() {
+    expect:
+    Thread.sleep(1000)
+    println("=--= hello test2 " + LocalDateTime.now())
+    true
   }
 
 
